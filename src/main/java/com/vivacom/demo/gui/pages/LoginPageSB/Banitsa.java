08347b5e62975446f9dc6c880c84e4118db41833
deyan-bahrin-upnetix <50741170+deyan-bahrin-upnetix@users.noreply.github.com>
2020-02-04 20:58:10 +0200

    Update Banitsa.java

--- src/main/java/com/vivacom/demo/gui/pages/LoginPageSB/Banitsa.java
+++ src/main/java/com/vivacom/demo/gui/pages/LoginPageSB/Banitsa.java
@@ -28,17 +28,4 @@ public class Banitsa extends AbstractPage {
         return new BanitsaOnBites(getDriver());
     }
 
-    public void addProductsToCart(List<String> productsToOrder) {
-
-        for (String productToOrder : productsToOrder) {
-            for(ExtendedWebElement existingProduct : existingProducts){
-                if(productToOrder.equals(existingProduct.findExtendedWebElement(By.cssSelector("h2")).getText())){
-                    existingProduct.findExtendedWebElement(By.cssSelector("a.add-to-cart")).click();
-                }
-            }
-
-        }
-
-    }
-
 }
